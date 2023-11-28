4e009bbc81f7f3fdf73e28ac55fd6a5a6e8d5ae5
nandinaulfaldi <nandinaufaldi@gmail.com>
2023-11-28 23:16:40 +0700

    fix on off

--- src/app/data/homepages/slidermenu.js
+++ src/app/data/homepages/slidermenu.js
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 
 
-export default function SliderMenu() {
+export default function SliderMenu({flag}) {
+  const display = flag === 1 ? {display:'block'} : {display:'none'}
     return (
       <>
        <div className="sliderMenuContainer">
-         <div className='sliderProductContainer'>
+         <div className='sliderProductContainer' style={display}>
            <div className='productContainer'>
               <div>
                 <h3 className='tittleH3'>Autoclavable Goggles</h3>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
+'use client'
 import styles from './data/css/home.css';
+import { useState } from 'react'; 
 import SliderMenu from './data/homepages/slidermenu.js';
 import Sliderproduct from './data/homepages/sliderproduct.js';
 import Info from './data/homepages/info.js';
 import Aboutus from './data/homepages/aboutus.js';
 import Footer from './data/homepages/footer.js';
 
+
 export default function Home() {
+  const [flag, setFlag] = useState(0);
+
+  const handleClick = (e) =>  {
+
+    if(flag === 0){
+      setFlag(1)
+    }else{
+      setFlag(0)
+    }
+
+  };
+
   return (
     <>
       <div className=" navBarContainer" >
@@ -23,7 +38,7 @@ export default function Home() {
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Produk</a>
+                  <a href="#" onClick={handleClick} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Produk</a>
                 </li>
                 <li>
                   <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Info</a>
@@ -41,7 +56,7 @@ export default function Home() {
       </div>
 
       <div className='container sliderMenuContent'>
-        <SliderMenu/>
+        <SliderMenu flag={flag}/>
       </div>
 
       <div className='products'>
